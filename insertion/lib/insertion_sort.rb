cfb32b64f101df1d0972db34a99a7caca8820611
Nikhil Shahi <shniks@gmail.com>
2022-08-05 13:30:07 -0700

    Refactored insertion sort code as prior version was causing an error in edge cases.

--- insertion/lib/insertion_sort.rb
+++ insertion/lib/insertion_sort.rb
@@ -9,16 +9,30 @@ class InsertionSort
   end
 
   def iteration(unsorted, sorted)
-    first = unsorted.first # First element in unsorted array
-    sorted << first if sorted.empty?
-    insert(sorted, first) if !sorted.empty?
+    if sorted.empty?
+      sorted_empty(unsorted, sorted)
+    else
+      sorted_not_empty(unsorted, sorted)
+    end
     pull_element_from_unsorted(unsorted)
+  end
+
+  def sorted_empty(unsorted, sorted)
+    sorted << unsorted.first
+  end
+
+  def sorted_not_empty(unsorted, sorted)
+    first = unsorted.first # First element in unsorted array
+    insert(sorted, first)
   end
 
   def insert(sorted, first)
     sorted.each_with_index do |value, index|
       if value > first
         sorted.insert(index, first)
+        break
+      elsif index + 1 == sorted.length
+        sorted.push(first)
         break
       end
     end
