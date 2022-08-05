c7b3e0e7e5c196431db656fcbd1ec6c8f3fc675a
Nikhil Shahi <shniks@gmail.com>
2022-08-05 11:52:03 -0700

    Created iteration sub method for the insertion sort method.

--- insertion/lib/insertion_sort.rb
+++ insertion/lib/insertion_sort.rb
@@ -1,4 +1,28 @@
+require 'pry'
+
 class InsertionSort
 
+  def sort(unsorted)
+    sorted = []
+    unsorted.length.times do
+      iteration(unsorted, sorted)
+    end
+    sorted
+  end
 
-end 
+  def iteration(unsorted, sorted)
+    first = unsorted.first
+    if sorted.empty?
+      sorted << first
+    else
+      sorted.each_with_index do |v, i|
+        if v > first
+          sorted.insert(i, first)
+          break
+        end
+      end
+    end
+    unsorted.shift
+  end
+
+end
