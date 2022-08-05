1ffb3027810d5be62eb0e603a9c4fa617ac6aa73
Nikhil Shahi <shniks@gmail.com>
2022-08-05 09:18:52 -0700

    Created a test to see if it can bubble sort an array of 8 numbers.

--- bubble/test/bubble_sort_test.rb
+++ bubble/test/bubble_sort_test.rb
@@ -13,10 +13,18 @@ class BubbleSortTest < Minitest::Test
     assert_instance_of BubbleSort, sorter
   end
 
-  def test_if_it_can_bubble_sort_an_array_of_5_elements
+  def test_if_it_can_bubble_sort_an_array_of_4_elements
     sorter = BubbleSort.new
 
     assert_equal ["a", "b", "c", "d"], sorter.sort(["d", "b", "a", "c"])
+  end
+
+  def test_if_it_can_bubble_sort_an_array_of_8_numbers
+    sorter = BubbleSort.new
+    input = [23, 34, 12, 74, 2, 19, 11, 14]
+    result = [2, 11, 12, 14, 19, 23, 34, 74]
+
+    assert_equal result, sorter.sort(input)
   end
 
 end
