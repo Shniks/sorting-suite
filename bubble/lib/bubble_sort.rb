2c66c17b84face0a86d4212f2c49d6a2e7a875c1
Nikhil Shahi <shniks@gmail.com>
2022-08-05 09:50:53 -0700

    Changed params name for the input array.

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -1,21 +1,21 @@
 class BubbleSort
 
-  def sort(input)
-    input.length.times do |s = 0|
-      iteration(input)
+  def sort(unsorted)
+    unsorted.length.times do |s = 0|
+      iteration(unsorted)
     end
-    input
+    unsorted
   end
 
-  def iteration(input)
-    (input.length - 1).times do |i = 0|
-      swap(input, i)
+  def iteration(unsorted)
+    (unsorted.length - 1).times do |i = 0|
+      swap(unsorted, i)
     end
   end
 
-  def swap(input, i)
-    if input[i+1] < input[i]
-      input[i], input[i+1] = input[i+1], input[i]
+  def swap(unsorted, i)
+    if unsorted[i+1] < unsorted[i]
+      unsorted[i], unsorted[i+1] = unsorted[i+1], unsorted[i]
     end
   end
 
