74dd707b2e2f08ac4f087738ca2ccedac766bb85
Nikhil Shahi <shniks@gmail.com>
2022-08-05 09:25:10 -0700

    Updated test file with a test to bubble sort an array of 15 numbers.

--- bubble/test/bubble_sort_test.rb
+++ bubble/test/bubble_sort_test.rb
@@ -35,4 +35,12 @@ class BubbleSortTest < Minitest::Test
     assert_equal result, sorter.sort(input)
   end
 
+  def test_if_it_can_bubble_sort_an_array_of_15_numbers
+    sorter = BubbleSort.new
+    input = [48, 58, 37, 31, 52, 99, 19, 1, 22, 32, 74, 37, 30, 43, 5]
+    result = [1, 5, 19, 22, 30, 31, 32, 37, 37, 43, 48, 52, 58, 74, 99]
+
+    assert_equal result, sorter.sort(input)
+  end
+
 end
