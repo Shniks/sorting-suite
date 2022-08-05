346df6f8ff2a038d80a28081892467f06be25e30
Nikhil Shahi <shniks@gmail.com>
2022-08-05 09:19:11 -0700

    Updated sort method to bubble sort.

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -1,4 +1,25 @@
+require 'pry'
+
 class BubbleSort
 
+  def sort(input)
+    i = 0
+    s = 0
+    total = input.length - 1
+    while s < input.length
+      s = s + 1
+      total.times do |i|
+        previous = input[i]
+        current = input[i+1]
+        if current < previous
+          input[i] = current
+          input[i+1] = previous
+        else
+        end
+        i = i + 1
+      end
+    end
+    input 
+  end
 
-end 
+end
