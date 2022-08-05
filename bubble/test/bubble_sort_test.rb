278b5b4aeb777c9aa87da99f0fee39bdf09f553d
Nikhil Shahi <shniks@gmail.com>
2022-08-05 07:38:19 -0700

    modified Rakefile to reflect correct path.

---
+++ bubble/test/bubble_sort_test.rb
@@ -0,0 +1,16 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/bubble_sort'
+
+class BubbleSortTest < Minitest::Test
+
+  def test_if_it_exists
+    sorter = BubbleSort.new
+
+    assert_instance_of BubbleSort, sorter
+  end 
+
+end
