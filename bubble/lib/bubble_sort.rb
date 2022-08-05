0188ccdb709632bac3399cdba7afcd12fa921269
Nikhil Shahi <shniks@gmail.com>
2022-08-05 09:48:24 -0700

    Refactored BubbleSort class to create separate methods. Completed Bubble Sort.

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -1,25 +1,22 @@
-require 'pry'
-
 class BubbleSort
 
   def sort(input)
-    i = 0
-    s = 0
-    total = input.length - 1
-    while s < input.length
-      s = s + 1
-      total.times do |i|
-        previous = input[i]
-        current = input[i+1]
-        if current < previous
-          input[i] = current
-          input[i+1] = previous
-        else
-        end
-        i = i + 1
-      end
+    input.length.times do |s = 0|
+      iteration(input)
     end
-    input 
+    input
+  end
+
+  def iteration(input)
+    (input.length - 1).times do |i = 0|
+      swap(input, i)
+    end
+  end
+
+  def swap(input, i)
+    if input[i+1] < input[i]
+      input[i], input[i+1] = input[i+1], input[i]
+    end
   end
 
 end
