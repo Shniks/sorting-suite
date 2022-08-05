be669527941fedc7a11b93dfd1ea1eaeb689c533
Nikhil Shahi <shniks@gmail.com>
2022-08-05 11:54:21 -0700

    Created an insertion test to sort an array of 15 numbers.

--- insertion/test/insertion_sort_test.rb
+++ insertion/test/insertion_sort_test.rb
@@ -35,4 +35,12 @@ class InsertionSortTest < Minitest::Test
     assert_equal result, sorter.sort(unsorted)
   end
 
+  def test_if_it_can_insertion_sort_an_array_of_15_numbers
+    sorter = BubbleSort.new
+    unsorted = [48, 58, 37, 31, 52, 99, 19, 1, 22, 32, 74, 37, 30, 43, 5]
+    result = [1, 5, 19, 22, 30, 31, 32, 37, 37, 43, 48, 52, 58, 74, 99]
+
+    assert_equal result, sorter.sort(unsorted)
+  end
+
 end
