4f08f40b49c62d897e830a2c633b8364721fe652
Nikhil Shahi <shniks@gmail.com>
2022-08-05 12:17:54 -0700

    Refactored the insertionsort class and broke the sort method into sub methods. Completed Insertion Sort.

--- insertion/lib/insertion_sort.rb
+++ insertion/lib/insertion_sort.rb
@@ -1,27 +1,30 @@
-require 'pry'
-
 class InsertionSort
 
   def sort(unsorted)
     sorted = []
-    unsorted.length.times do
+    until unsorted.empty?
       iteration(unsorted, sorted)
     end
     sorted
   end
 
   def iteration(unsorted, sorted)
-    first = unsorted.first
-    if sorted.empty?
-      sorted << first
-    else
-      sorted.each_with_index do |v, i|
-        if v > first
-          sorted.insert(i, first)
-          break
-        end
+    first = unsorted.first # First element in unsorted array
+    sorted << first if sorted.empty?
+    insert(sorted, first) if !sorted.empty?
+    pull_element_from_unsorted(unsorted)
+  end
+
+  def insert(sorted, first)
+    sorted.each_with_index do |value, index|
+      if value > first
+        sorted.insert(index, first)
+        break
       end
     end
+  end
+
+  def pull_element_from_unsorted(unsorted)
     unsorted.shift
   end
 
